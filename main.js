48ccdbe1b48871d789817c2b73f1dd63cf8c7db0
octoshrimpy <octavo.acapella@gmail.com>
2022-02-21 22:59:57 -0700

    beginnings

---
+++ main.js
@@ -0,0 +1,37 @@
+let data = {
+    "worlds": {
+        "a": {
+            "cities": {
+                "deiro":{},
+                "fholm":{},
+                "pitwell":{}
+            }
+        },
+        "b": {
+            "cities": {
+                "westfield":{},
+                "thampton":{},
+                "ottle":{}
+            }
+        }
+    }
+}
+
+function loadData () {
+    // get data from browserstorage
+
+    return data;
+}
+
+function searchFor (item) {
+    console.log(item)
+    return item
+}
+
+function search () {
+    let searchTerm = searchbar.value 
+    let item = searchFor(searchTerm)
+}
+
+let searchbar = document.querySelector("input.bar")
+searchbar.addEventListener("onchange", search );
