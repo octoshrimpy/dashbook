ce3fb9a6d6f0a446ae275ff17ec7a2fbbe5a30a2
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-03-18 21:03:04 -0600

    Extracted entries

---
+++ cells/entries.js
@@ -0,0 +1,94 @@
+Entry.loadFromJSON([
+  {
+    name: "WorldA",
+    type: "place",
+    subtype: "world",
+    children: [
+      {
+        name: "jurgaen",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city jurgaen",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      },
+      {
+        name: "th'rom",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city th'rom",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      },
+      {
+        name: "wryze-all",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city wryze-all",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "WorldB",
+    type: "place",
+    subtype: "world",
+    children: [
+      {
+        name: "jurgaen",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city jurgaen",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      },
+      {
+        name: "th'rom",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city th'rom",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      },
+      {
+        name: "wryze-all",
+        type: "place",
+        subtype: "region",
+        children: [
+          {
+            name: "city wryze-all",
+            type: "place",
+            subtype: "city",
+            children: [],
+          }
+        ]
+      }
+    ]
+  }
+])
