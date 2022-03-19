323362df286a6216fc237b047ed4dcfb55928b1d
octoshrimpy <octavo.acapella@gmail.com>
2022-03-18 21:43:17 -0600

    extra content

--- cells/entries.js
+++ cells/entries.js
@@ -16,7 +16,26 @@ Entry.loadFromJSON([
             summary: "treehouse town just below the high canopies",
             type: "place",
             subtype: "city",
-            children: [],
+            children: [
+              {
+                name: "Tilted Anvil",
+                summary: "Blacksmith shop owned by  Nathaniel",
+                type: "place",
+                subtype: "shop",
+              },
+              {
+                name: "The Book Worm",
+                summary: "town library & bookstore",
+                type: "place",
+                subtype: ["shop", "service"],
+              },
+              {
+                name: "Gruggenheim",
+                summary: "tavern. blackened stone, ran by goblins",
+                type: "place",
+                subtype: "shop",
+              }
+            ],
           }
         ]
       },
@@ -31,7 +50,26 @@ Entry.loadFromJSON([
             summary: "city deep underground, filling old empty lava tubes",
             type: "place",
             subtype: "city",
-            children: [],
+            children: [
+              {
+                name: "Ginger-Vitae",
+                summary: "cybernetic body enhancement parlor",
+                type: "place",
+                subtype: "shop",
+              },
+              {
+                name: "duck pond",
+                summary: "a medium sized pond. it has ducks.",
+                type: "place",
+                subtype: "service",
+              },
+              {
+                name: "Chapel of the fifth sky",
+                summary: "church ran by an underground cult. they pay taxes.",
+                type: "place",
+                subtype: "entertainment",
+              }
+            ],
           }
         ]
       },
@@ -46,7 +84,26 @@ Entry.loadFromJSON([
             summary: "town of nomads, traversing the icy plains following the seals",
             type: "place",
             subtype: "city",
-            children: [],
+            children: [
+              {
+                name: "tictockery",
+                summary: "shop filled with gizmos run by local artificer",
+                type: "place",
+                subtype: "shop",
+              },
+              {
+                name: "sheriff's office",
+                summary: "sheriff Cunningham works here",
+                type: "place",
+                subtype: "service",
+              },
+              {
+                name: "a bard's tail",
+                summary: "the town's local theatre. also used for magic duels",
+                type: "place",
+                subtype: "entertainment",
+              }
+            ],
           }
         ]
       }
